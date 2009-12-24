3cb0299476771d85e5877a7d5865276428891144
Nathan Wright <nathan@brandalism.com>
2009-12-23 18:02:09 -0600

    Got doctor rotator working.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,4 +12,53 @@ $(document).ready(function() {
     var parent_height = $(this).parent().height();
     $(this).height(parent_height);
   });
+  
+  
+  
+  // Physician rotator
+  var doctor_excerpts = $('#doctor_excerpt li');
+  doctor_excerpts.css('display', 'none');
+  doctor_excerpts_max_height = 0;
+  doctor_excerpts.each(function(i) {
+    $(this).css('left', '0');
+    if ($(this).height() > doctor_excerpts_max_height) {
+      doctor_excerpts_max_height = $(this).height();
+    }
+  });
+  doctor_excerpts.parent().height(doctor_excerpts_max_height);
+  rotate_doctors = true;
+  doctor_excerpts.parent().bind("mouseenter", function(e) {
+    rotate_doctors = false;
+  });
+  doctor_excerpts.parent().parent().parent().bind("mouseleave", function(e) {
+    rotate_doctors = true;
+  });
+
+  function doctor_rotator() {
+    if (rotate_doctors != false) {
+      var current_doctor = $('#doctor_excerpt li:visible');
+      // var num = Math.floor(Math.random() * doctor_excerpts.length);
+      var num = smart_randomizer(null, doctor_excerpts.length);
+      var random_doctor = $('#doctor_excerpt li:eq('+ num +')');
+      current_doctor.fadeOut('slow');
+      // random_doctor.css('display', 'block');
+      random_doctor.fadeIn('slow');
+      // setTimeout('doctor_rotator', 1000);
+    }
+  }
+  
+  function smart_randomizer(current_number, max_number) {
+    current_number = typeof(current_number) != 'undefined' ? current_number : Math.floor(Math.random() * max_number);
+    var new_number = Math.floor(Math.random() * max_number);
+    if (new_number == current_number) {
+      smart_randomizer(current_number, max_number);
+    } else {
+      return new_number;
+    }
+  }
+  
+  doctor_rotator();
+  setInterval(function() {
+      doctor_rotator();
+  }, 5000);
 });
--- vendor/extensions/utility_tags/app/models/utility_tags.rb
+++ vendor/extensions/utility_tags/app/models/utility_tags.rb
@@ -1,10 +1,20 @@
+class ActiveRecord::Base
+  def escape_radius_tags(text)
+    text.gsub(/(<\/?r:.*?>)/) {|s| CGI.escape(s)}  # Nokogiri pukes on radiant tags, have to comment them out
+  end
+  
+  def unescape_radius_tags(nokogiri_doc)
+    nokogiri_doc.to_s.gsub(/%3C(%2F)?r%3A(.*?)%3E/) {|s| CGI.unescape(s) } # re-convert <r:whatever> tags
+  end
+end
+
 module UtilityTags
   include Radiant::Taggable
 
   class TagError < StandardError; end
   
   desc %{
-    Splits a URL into various sections, delimited by a character. Useful for pulling using as a filter.
+    Splits a URL into various sections, delimited by a character. Useful for displaying just part of a URL string.
 
     *Usage:*
     
@@ -38,7 +48,7 @@ module UtilityTags
   }
   tag 'part' do |tag|
     tag.locals.part = tag.globals.part
-    raise tag.inspect
+    raise escape_radius_tags(tag.content).inspect
     tag.expand
   end
   
@@ -99,6 +109,140 @@ module UtilityTags
     end
     result
   end
+
+  desc %{
+    Grabs only a portion of a page part.
   
+    *Usage:*
+    
+    <pre><code><r:excerpt [name="body"] [num="15"] [truncate_string="..."] /></code></pre>
+  }
+  tag 'excerpt' do |tag|
+    # html_elements = tag.attr.has_key?('html_elements') ? tag.attr['html_elements'] : ['p']
+    name = tag.attr.has_key?('name') ? tag.attr['name'] : 'body'
+    num = tag.attr.has_key?('num_words') ? tag.attr['num'] : 100
+    truncate_string = tag.attr.has_key?('truncate_string') ? tag.attr['truncate_string'] : '...'
+
+    page = tag.locals.page
+    excerpt = page.part(name).content rescue nil
+    puts "----> #{excerpt.inspect}"
+    if excerpt
+      doc = Nokogiri::HTML.fragment(excerpt)
+      excerpted_text = doc.css('p')
+      excerpted_text.search('img').remove
+      unless excerpted_text.blank?
+        # truncated = html_truncate(excerpted_text.to_s.gsub(/<p>\s*&nbsp;\s*<\/p>/, ''), num_words.to_i, truncate_string).gsub(/<\/?.*?>/, '').gsub(/&amp;/, '&')
+        truncated = excerpted_text.to_s.gsub(/<p>\s*&nbsp;\s*<\/p>/, '').gsub(/<\/?.*?>/, '').gsub(/&amp;/, '&')[0..num.to_i]
+        content = unescape_radius_tags("<p>#{truncated}#{truncate_string}</p>")
+      end
+    end
+  end
+
+  tag 'excerpt_image' do |tag|
+    page = tag.locals.page
+    image = page.part('head_shot').content rescue nil
+    if image
+      noko_img = Nokogiri::HTML.fragment(image).css('img').first
+      img = %{<img src="#{noko_img.attribute('src')}" width="67" alt="#{noko_img.attribute('alt')}" />}
+    end
+    img
+  end
+  
+  tag 'children:each_if' do |tag|
+    raise TagError.new("`children:each_if' tag must contain a `name' attribute.") unless tag.attr.has_key?('name')
+    name = tag.attr['name']
+
+    page = tag.locals.page
+    raise page.parts.inspect
+    
+    tag.expand if tag.locals.first_child
+  end
+  
+  
+end
+
+def html_truncate(input, num = 15, truncate_string = "...")
+	doc = Nokogiri::HTML(input)
+
+	current = doc.children.first
+	count = 0
+
+	while true
+		# we found a text node
+		if current.is_a?(Nokogiri::XML::Text)
+			count += current.text.split.length
+			# we reached our limit, let's get outta here!
+			break if count > num
+			previous = current
+		end
+
+		if current.children.length > 0
+			# this node has children, can't be a text node,
+			# lets descend and look for text nodes
+			current = current.children.first
+		elsif !current.next.nil?
+			#this has no children, but has a sibling, let's check it out
+			current = current.next
+		else 
+			# we are the last child, we need to ascend until we are
+			# either done or find a sibling to continue on to
+			n = current
+			while !n.is_a?(Nokogiri::HTML::Document) and n.parent.next.nil?
+				n = n.parent
+			end
+
+			# we've reached the top and found no more text nodes, break
+			if n.is_a?(Nokogiri::HTML::Document)
+				break;
+			else
+				current = n.parent.next
+			end
+		end
+	end
+
+	if count >= num
+	  unless count == num
+  		new_content = current.text.split
+
+      # If we're here, the last text node we counted eclipsed the number of words
+      # that we want, so we need to cut down on words.  The easiest way to think about
+      # this is that without this node we'd have fewer words than the limit, so all
+      # the previous words plus a limited number of words from this node are needed.
+      # We simply need to figure out how many words are needed and grab that many.
+      # Then we need to -subtract- an index, because the first word would be index zero.
+
+      # For example, given:
+      # <p>Testing this HTML truncater.</p><p>To see if its working.</p>
+      # Let's say I want 6 words.  The correct returned string would be:
+      # <p>Testing this HTML truncater.</p><p>To see...</p>
+      # All the words in both paragraphs = 9
+      # The last paragraph is the one that breaks the limit.  How many words would we
+      # have without it? 4.  But we want up to 6, so we might as well get that many.
+      # 6 - 4 = 2, so we get 2 words from this node, but words #1-2 are indices #0-1, so
+      # we subtract 1.  If this gives us -1, we want nothing from this node. So go back to
+      # the previous node instead.
+      index = num-(count-new_content.length)-1
+      if index >= 0
+        new_content = new_content[0..index]
+  		  current.content = new_content.join(' ') + truncate_string
+		  else
+		    current = previous
+		    current.content = current.content + truncate_string
+	    end
+	  end
+
+		# remove everything else
+		while !current.is_a?(Nokogiri::HTML::Document)
+			while !current.next.nil?
+				current.next.remove
+			end
+			current = current.parent
+		end
+	end
+
+	# now we grab the html and not the text.
+	# we do first because nokogiri adds html and body tags
+	# which we don't want
+	doc.root.children.first.inner_html
 end
 
